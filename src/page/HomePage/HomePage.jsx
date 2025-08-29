e489b721c6de75e062a06728c323874e63ca11bc
nahn <crepvn2009@gmail.com>
2025-08-29 16:57:27 +0700

    neubronew

--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -9,9 +9,18 @@ const HomePage = () => {
       <Sidebar />
       <main>
         <div className='banner'>
-          <img src="" alt="" >
-            
-          </img>
+          <div className='banner-title'>
+            <h1>KATO Shop</h1>
+          </div>
+          <div className='about-card'>
+            <img src='' alt='' />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sed
+              laborum impedit dignissimos, doloremque porro illum earum veniam
+              soluta delectus voluptate inventore molestias atque perspiciatis
+              eum a reiciendis debitis? Error.
+            </p>
+          </div>
         </div>
       </main>
       <Footer />
